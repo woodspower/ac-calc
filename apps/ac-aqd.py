61d6cd7338470de46dbd6473f71857a56d6786ea
King Chung Huang <kinghuang@mac.com>
2021-11-14 14:36:38 -0700

    Show a table of Aeroplan distances by origin airport

--- apps/ac-aqd.py
+++ apps/ac-aqd.py
@@ -1,8 +1,53 @@
+import pandas as pd
 import streamlit as st
+
+from ac_aqd.data import AIRPORTS, COUNTRIES, DISTANCES
 
 
 def main():
-    st.text("Hello, world!")
+    st.set_page_config(page_title="AC Aeroplan Calculator", layout="wide")
+
+    tools = {
+        "Calculate SQM and SQD": calculate_sqm_sqd,
+        "Browse Distances": browse_distances,
+    }
+    tool_title = st.sidebar.radio("Tool", tools.keys())
+    tool = tools[tool_title]
+    tool(tool_title)
+
+
+def calculate_sqm_sqd(title):
+    st.header(title)
+
+
+def browse_distances(title):
+    st.header(title)
+
+    origin = st.selectbox(
+        "Origin 🛫",
+        AIRPORTS,
+        # index=DEFAULT_ORIGIN_AIRPORT_INDEX,
+        index=0,
+        format_func=lambda airport: airport.iata_code,
+        help="Flight origin airport code.",
+    )
+
+    destinations = []
+    old_distances = []
+    new_distances = []
+
+    for _, distance in origin.distances.items():
+        destinations.append(distance.destination)
+        old_distances.append(distance.old_distance)
+        new_distances.append(distance.distance)
+
+    distances_df = pd.DataFrame({
+        "destination": destinations,
+        "old": old_distances,
+        "new": new_distances,
+    })
+
+    st.table(distances_df)
 
 
 if __name__ == "__main__":
