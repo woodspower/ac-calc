cc7bd3c20c6a8ccc1786b89e1da84edec1ff161c
King Chung Huang <kinghuang@mac.com>
2021-11-27 12:54:29 -0700

    Fix declaration of fare brands
    Remove the trailing commas.

--- ac_aqd/aeroplan.py
+++ ac_aqd/aeroplan.py
@@ -23,16 +23,16 @@ AEROPLAN_STATUSES = (
 )
 
 
-NoBrand = FareBrand("None", tuple(), [c for c in "JCDZPOENYBMUHQVWGSTLAK"], 0.0, 0.0),
-Basic = FareBrand("Basic", ("BA", "GT"), [c for c in "YBMUHQVWGSTLAK"], 0.25, 0.25),
-Standard = FareBrand("Standard", ("TG",), [c for c in "YBMUHQVWGSTLAK"], 0.5, 0.25),
-Flex = FareBrand("Flex", ("FL",), [c for c in "YBMUHQVWGSTLAK"], 1.0, 1.0),
-Comfort = FareBrand("Comfort", ("CO",), [c for c in "YBMUHQVWGSTLAK"], 1.15, 1.15),
-Latitude = FareBrand("Latitude", ("LT",), [c for c in "YBMUHQVWGSTLAK"], 1.25, 1.25),
-PremiumEconomyLowest = FareBrand("Premium Economy (Lowest)", ("PL",), ["O", "E", "N"], 1.25, 1.25),
-PremiumEconomyFlexible = FareBrand("Premium Economy (Flexible)", ("PF",), ["O", "E", "N"], 1.25, 1.25),
-BusinessLowest = FareBrand("Business (Lowest)", ("EL",), ["J", "C", "D", "Z", "P"], 1.50, 1.50),
-BusinessFlexible = FareBrand("Business (Flexible)", ("EF",), ["J", "C", "D", "Z", "P"], 1.50, 1.50),
+NoBrand = FareBrand("None", tuple(), [c for c in "JCDZPOENYBMUHQVWGSTLAK"], 0.0, 0.0)
+Basic = FareBrand("Basic", ("BA", "GT"), [c for c in "YBMUHQVWGSTLAK"], 0.25, 0.25)
+Standard = FareBrand("Standard", ("TG",), [c for c in "YBMUHQVWGSTLAK"], 0.5, 0.25)
+Flex = FareBrand("Flex", ("FL",), [c for c in "YBMUHQVWGSTLAK"], 1.0, 1.0)
+Comfort = FareBrand("Comfort", ("CO",), [c for c in "YBMUHQVWGSTLAK"], 1.15, 1.15)
+Latitude = FareBrand("Latitude", ("LT",), [c for c in "YBMUHQVWGSTLAK"], 1.25, 1.25)
+PremiumEconomyLowest = FareBrand("Premium Economy (Lowest)", ("PL",), ["O", "E", "N"], 1.25, 1.25)
+PremiumEconomyFlexible = FareBrand("Premium Economy (Flexible)", ("PF",), ["O", "E", "N"], 1.25, 1.25)
+BusinessLowest = FareBrand("Business (Lowest)", ("EL",), ["J", "C", "D", "Z", "P"], 1.50, 1.50)
+BusinessFlexible = FareBrand("Business (Flexible)", ("EF",), ["J", "C", "D", "Z", "P"], 1.50, 1.50)
 
 
 FARE_BRANDS = (
